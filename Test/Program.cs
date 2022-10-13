8723bc95a8a8e2ea45b7bcef0179933ccfa9de3d
Rakan8181 <112816380+Rakan8181@users.noreply.github.com>
2022-10-13 15:40:47 +0100

    Test

--- Test/Program.cs
+++ Test/Program.cs
@@ -10,26 +10,51 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Task1();
+            Task2();
             Console.ReadLine();
 
         }
         static void Task1()
         {
-            int n;
-            n = Convert.ToInt16(Console.ReadLine());
+            int n = Convert.ToInt16(Console.ReadLine());
             int result = n;
             int remainder = 0;
             string binary_num = null;
+            string reverse_binary_num = null;
             do
             {
-                result = result / 2;
-                Console.WriteLine($"The result if {result}");
                 remainder = result % 2;
-                Console.Write($"The remainder is {remainder}");
+                result = result / 2;
                 binary_num += remainder;
             } while (result != 0);
-            Console.Write(binary_num);
+            for (int i = binary_num.Length - 1; i > -1; i--)
+            {
+                reverse_binary_num += binary_num[i];
+            }
+            Console.WriteLine(reverse_binary_num);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Enter a whole number: ");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another whole number: ");
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+            int Temp1 = Number1;
+            int Temp2 = Number2;
+            while (Temp1 != Temp2)
+            {
+                if (Temp1 > Temp2)
+                {
+                    Temp1 = Temp1 - Temp2;
+                }
+                else
+                {
+                    Temp2 = Temp2 - Temp1;
+                }
+            }
+            int Result = Temp1;
+            Console.WriteLine($"{Result} is GCF of {Number1} and {Number2}");
+
         }
     }
 }
